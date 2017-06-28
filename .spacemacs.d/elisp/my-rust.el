87b4c09e0fa78cf98d234022dde7b49a3b054900
wariuni <wariuni@gmail.com>
2017-06-28 15:12:08 +0900

    　

--- .spacemacs.d/elisp/my-rust.el
+++ .spacemacs.d/elisp/my-rust.el
@@ -13,28 +13,21 @@
   (evil-define-key 'normal rust-mode-map "\C-q" 'racer-describe)
   (evil-define-key 'normal rust-mode-map "\C-]" 'racer-find-definition)
   (evil-define-key 'normal rust-mode-map "\M-r" 'my-rust-run)
-  (evil-define-key 'normal rust-mode-map "\M-t" 'my-rust-test-all-or-bin))
+  (evil-define-key 'normal rust-mode-map "\M-t" 'cargo-process-test))
 
 (defun my-rust-run ()
   (interactive)
   (let ((file-path (buffer-file-name)))
     (cond ((string-match ".*/rust-contest/src/bin/\\(.+\\)\\.rs" file-path)
-           (let ((buffer (get-buffer "*cargo-judge*")))
+           (let ((buffer (get-buffer "*snowchains*")))
              (when buffer
                (with-current-buffer buffer
                  (erase-buffer))))
-           (term-run "sh" "*cargo-judge*" "-c" (concat "cargo judge " (match-string 1 file-path))))
+           (let ((problem (match-string 1 file-path)))
+             (term-run "sh" "*snowchains*" "-c" (format "snowchains cargo judge %s %s.toml" problem problem))))
           ((string-match ".*/src/bin/\\(.+\\)\\.rs" file-path)
            (cargo-process-run-bin (match-string 1 file-path)))
           (t
            (cargo-process-run)))))
-
-(defun my-rust-test-all-or-bin ()
-  (interactive)
-  (let ((file-name (buffer-file-name)))
-    (cond ((string-match ".*/src/bin/\\(.+\\)\\.rs" file-name)
-           (cargo-process--start "Test" (format "cargo test --release --bin %s" (match-string 1 file-name))))
-          (t
-           (cargo-process-test)))))
 
 (add-hook 'rust-mode-hook 'my-rust-init)
