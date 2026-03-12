b57b8bb79c00b0d4d587602c8a06beb5b1baedc7
Ryo Yamashita <qryxip@gmail.com>
2026-03-12 23:25:09 +0900

    feat(nvim): `set colorcolumn=72` for COMMIT_EDITMSG

--- common/ln/homedir/.config/nvim/init.lua
+++ common/ln/homedir/.config/nvim/init.lua
@@ -114,6 +114,13 @@ vim.api.nvim_create_autocmd({ 'FileType' }, {
   end,
 })
 
+vim.api.nvim_create_augroup('on_gitcommit', { clear = true })
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'gitcommit',
+  group = 'on_gitcommit',
+  command = 'set colorcolumn=72',
+})
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
